78723026f9952e17dc8dd1e9c50038490fbc2293
Miguel Angel Torres Ramírez <102279255+mig-elmo07@users.noreply.github.com>
2022-06-16 20:23:47 -0500

    Add files via upload

---
+++ piano.js
@@ -0,0 +1,201 @@
+(function() {
+
+  /* Piano keyboard pitches. Names match sound files by ID attribute. */
+  
+  var keys =[
+    'C2', 'Db2', 'D2', 'Eb2', 'E2', 'F2', 'Gb2', 'G2', 'Ab2', 'A2', 'Bb2', 'B2',
+    'C3', 'Db3', 'D3', 'Eb3', 'E3', 'F3', 'Gb3', 'G3', 'Ab3', 'A3', 'Bb3', 'B3',
+    'C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4',
+    'C5', 'Db5', 'D5', 'Eb5', 'E5', 'F5', 'Gb5', 'G5', 'Ab5', 'A5', 'Bb5', 'B5',
+    'C6', 'Db6', 'D6', 'Eb6', 'E6', 'F6', 'Gb6', 'G6', 'Ab6', 'A6', 'Bb6', 'B6',
+    'C7',
+  ];
+
+  /* Corresponding keyboard keycodes, in order w/ 'keys'. */
+  /* QWERTY layout:
+  /*   upper register: Q -> P, with 1-0 as black keys. */
+  /*   lower register: Z -> M, , with A-L as black keys. */
+  
+  var codes = [
+     7,     7,     7,    7,    7,     7,    7,    7,     7,    7,     7,     7,
+     7,     7,     7,    7,    7,     7,    7,    7,     7,    7,     7,     7,
+     81,   50,    87,   51,   69,    82,   53,   84,    54,   89,    55,    85,
+     73,   57,    79,   48,   80,    90,   83,   88,    68,   67,    70,    86,
+     66,   72,    78,   74,   77,     7,    7,    7,     7,    7,     7,     7,
+     7,
+  ];
+  
+  var pedal = 32; /* Keycode for sustain pedal. */
+  var tonic = 'C2'; /* Lowest pitch. */
+  
+  /* Piano state. */
+  
+  var intervals = {};
+  var depressed = {};
+  
+  /* Selectors */
+  
+  function pianoClass(name) {
+    return '.piano-' + name;
+  };
+  
+  function soundId(id) {
+    return 'sound-' + id;
+  };
+  
+  function sound(id) {
+    var it = document.getElementById(soundId(id));
+    return it;
+  };
+
+  /* Virtual piano keyboard events. */
+  
+  function keyup(code) {
+    var offset = codes.indexOf(code);
+    var k;
+    if (offset >= 0) {
+      k = keys.indexOf(tonic) + offset;
+      return keys[k];
+    }
+  };
+  
+  function keydown(code) {
+    return keyup(code);
+  };
+  
+  function press(key) {
+    var audio = sound(key);
+    if (depressed[key]) {
+      return;
+    }
+    clearInterval(intervals[key]);
+    if (audio) {
+      audio.pause();
+      audio.volume = 1.0;
+      if (audio.readyState >= 2) {
+        audio.currentTime = 0;
+        audio.play();
+        depressed[key] = true;
+      }
+    }
+    $(pianoClass(key)).animate({
+    'backgroundColor': '#c32955'
+    }, 0);
+  };
+
+  /* Manually diminish the volume when the key is not sustained. */
+  /* These values are hand-selected for a pleasant fade-out quality. */
+  
+  function fade(key) {
+    var audio = sound(key);
+    var stepfade = function() {
+      if (audio) {
+        if (audio.volume < 0.03) {
+          kill(key)();
+        } else {
+          if (audio.volume > 0.2) {
+            audio.volume = audio.volume * 0.95;
+          } else {
+            audio.volume = audio.volume - 0.01;
+          }
+        }
+      }
+    };
+    return function() {
+      clearInterval(intervals[key]);
+      intervals[key] = setInterval(stepfade, 5);
+    };
+  };
+
+  /* Bring a key to an immediate halt. */
+  
+  function kill(key) {
+    var audio = sound(key);
+    return function() {
+      clearInterval(intervals[key]);
+      if (audio) {
+        audio.pause();
+      }
+      if (key.length > 2) {
+        $(pianoClass(key)).animate({
+          'backgroundColor': 'black'
+        }, 300, 'easeOutExpo');
+      } else {
+        $(pianoClass(key)).animate({
+          'backgroundColor': 'white'
+        }, 300, 'easeOutExpo');
+      }
+    };
+  };
+
+  /* Simulate a gentle release, as opposed to hard stop. */
+  
+  var fadeout = true;
+
+  /* Sustain pedal, toggled by user. */
+  
+  var sustaining = false;
+
+  /* Register mouse event callbacks. */
+  
+  keys.forEach(function(key) {
+    $(pianoClass(key)).mousedown(function() {
+      $(pianoClass(key)).animate({
+        'backgroundColor': '#c32955'
+      }, 0);
+      press(key);
+    });
+    if (fadeout) {
+      $(pianoClass(key)).mouseup(function() {
+        depressed[key] = false;
+        if (!sustaining) {
+          fade(key)();
+        }
+      });
+    } else {
+      $(pianoClass(key)).mouseup(function() {
+        depressed[key] = false;
+        if (!sustaining) {
+          kill(key)();
+        }
+      });
+    }
+  });
+
+  /* Register keyboard event callbacks. */
+  
+  $(document).keydown(function(event) {
+    if (event.which === pedal) {
+      sustaining = true;
+      $(pianoClass('pedal')).addClass('piano-sustain');
+    }
+    press(keydown(event.which));
+  });
+  
+  $(document).keyup(function(event) {
+    if (event.which === pedal) {
+      sustaining = false;
+      $(pianoClass('pedal')).removeClass('piano-sustain');
+      Object.keys(depressed).forEach(function(key) {
+        if (!depressed[key]) {
+          if (fadeout) {
+            fade(key)();
+          } else {
+            kill(key)();
+          }
+        }
+      });
+    }
+    if (keyup(event.which)) {
+      depressed[keyup(event.which)] = false;
+      if (!sustaining) {
+        if (fadeout) {
+          fade(keyup(event.which))();
+        } else {
+          kill(keyup(event.which))();
+        }
+      }
+    }
+  });
+
+})();
